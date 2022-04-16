37a2fea0f984b151c4b51d6d504673c7d00a6340
Felipe Toffoli <felipe_toffoli1@hotmail.com>
2022-04-16 18:59:19 -0400

    init back nestjs

---
+++ back/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: false,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+     UsersModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
---
+++ back/src/users/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto {
+    email: String;
+    username: String;
+    password: String;
+    cpf: String;
+
+}
