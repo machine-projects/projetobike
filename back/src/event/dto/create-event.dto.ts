05c742dace0c13709b58e9fbf0482a74d7f8f271
Felipe Toffoli <felipe_toffoli1@users.noreply.github.com>
2022-05-12 16:16:41 -0400

    dto e add repository tho service event

--- back/src/event/dto/create-event.dto.ts
+++ back/src/event/dto/create-event.dto.ts
@@ -1,1 +1,13 @@
-export class CreateEventDto {}
+import { isDate, isNotEmpty } from "class-validator";
+
+export class CreateEventDto {
+  @isNotEmpty()
+  title: string;
+
+  description: string;
+
+  @isDate()
+  startDate: Date;
+  @isDate()
+  stopDate: Date;
+}
--- back/src/event/event.service.ts
+++ back/src/event/event.service.ts
@@ -4,31 +4,34 @@ import { Repository } from 'typeorm';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { EventEntity } from './entities/event.entity';
+import { FeaturedEventEntity } from './entities/featuredEvent.entity';
 
 @Injectable()
 export class EventService {
   constructor(
     @InjectRepository(EventEntity)
     private readonly eventRepository: Repository<EventEntity>,
+    private readonly featuredEventEntity: Repository<FeaturedEventEntity>,
   ) {}
 
-  create(createEventDto: CreateEventDto) {
+  async create(createEventDto: CreateEventDto) {
+    await this.eventRepository.create(createEventDto)
     return 'This action adds a new event';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all event`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} event`;
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
+  async update(id: number, updateEventDto: UpdateEventDto) {
     return `This action updates a #${id} event`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} event`;
   }
 }
