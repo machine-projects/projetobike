f653619be5fa009dbb7eb3c5782474a50339d2c8
arthurnassar <arthurnassar@gmail.com>
2022-05-24 08:04:44 -0400

    atualização do front

---
+++ front/website/src/views/LoginAtleta.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <NavBar/>
+  <div id="atleta-page" class="d-flex flex-column justify-content-center">
+        <SignRegisterForm />
+  </div>
+  </div>
+</template>
+
+<script>
+ import SignRegisterForm from '@/components/forms/SignRegisterForm.vue'
+ import NavBar from '@/components/NavBar.vue'
+
+  export default {
+    name: 'LoginAtleta',
+    components: {
+      SignRegisterForm,
+      NavBar
+    }
+  }
+</script>
+
+<style scoped>
+  #atleta-page {
+    height: calc(100vh - 58px);
+  }
+</style>
---
+++ front/website/src/views/LoginOrganizador.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <NavBar/>
+  <div id="organizador-page" class="d-flex flex-column justify-content-center">
+      <SignRegisterForm />
+  </div> 
+  </div>
+</template>
+
+<script>
+  import SignRegisterForm from '@/components/forms/SignRegisterForm.vue'
+  import NavBar from '@/components/NavBar.vue'
+
+  export default {
+    name: 'LoginOrganizador',
+    components: {
+      SignRegisterForm,
+      NavBar
+    }
+  }
+</script>
+
+
+<style scoped>
+   #organizador-page {
+     height: calc(100vh - 58px);
+  }
+</style>
