8d32b542a7cf37a532db703f4321c8fab553b78b
Felipe Toffoli <felipe_toffoli1@users.noreply.github.com>
2022-05-24 13:06:11 -0400

    user admin

---
+++ back/admin-user.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: projetobike
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.6-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` varchar(36) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `createdDate` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedDate` datetime(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_97672ac88f789774dd47f7c8be` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('c1cbc41f-f122-4035-a392-20576325ea91','Administator','System','admin@machineprojects.com.br','82208816072','$2b$10$ksHL6fX4hKvfF5oS38hoUuvVSoTQeqBZO5pm1AvFX0.zlo5hcNFIe','2022-05-24 13:02:45.096927','2022-05-24 13:02:45.096927');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-24 13:05:03
--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindConditions, FindOneOptions, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersEntity } from './entities/user.entity';
 import { userMessage } from './messages/users.messages';
 
@@ -8,48 +9,70 @@ import { userMessage } from './messages/users.messages';
 export class UsersService {
   constructor(
     @InjectRepository(UsersEntity)
-    private readonly usersRepository: Repository<UsersEntity>
-
-  ) { }
-
-
+    private readonly usersRepository: Repository<UsersEntity>,
+  ) {}
 
   async create(createUserDto: any) {
-    if (await this.findOne({ email: createUserDto.email }))
-      throw new NotFoundException(userMessage.existEmail)
-    const user = await this.usersRepository.create(createUserDto)
+    if (await this.usersRepository.findOne({ email: createUserDto.email }))
+      throw new NotFoundException(userMessage.existEmail);
+    const user = await this.usersRepository.create(createUserDto);
     return await this.usersRepository.save(user);
   }
 
   async findAll() {
-    return await this.usersRepository.find({ select: ['id', 'cpf', 'firstName', 'lastName', 'email', 'createdDate', 'updatedDate'] });
+    return await this.usersRepository.find({
+      select: [
+        'id',
+        'cpf',
+        'firstName',
+        'lastName',
+        'email',
+        'createdDate',
+        'updatedDate',
+      ],
+    });
   }
 
-  async findOne(
-    conditions: FindConditions<UsersEntity>,
-    options?: any) {
+  async findOne(conditions: FindConditions<UsersEntity>, options?: any) {
     try {
-
-      return this.usersRepository.findOneOrFail(conditions, options)
-    }
-    catch (error) {
-      throw new NotFoundException(error.message)
+      return this.usersRepository.findOneOrFail(conditions, options);
+    } catch (error) {
+      throw new NotFoundException(error.message);
     }
   }
 
   async findOneNotPass(conditions: FindConditions<UsersEntity>) {
     // { select: ['id', 'cpf', 'firstName', 'lastName', 'email', 'createdDate', 'updatedDate'] }
-    let options =  {select: ['id', 'cpf', 'firstName', 'lastName', 'email', 'createdDate', 'updatedDate'] };
-    
+    let options = {
+      select: [
+        'id',
+        'cpf',
+        'firstName',
+        'lastName',
+        'email',
+        'createdDate',
+        'updatedDate',
+      ],
+    };
+
     return await this.findOne(conditions, options);
   }
 
   async update(id: string, data) {
     const user = await this.findOneNotPass({ id });
     this.usersRepository.merge(user, data);
-    const options  = {select: ['id', 'cpf', 'firstName', 'lastName', 'email', 'createdDate', 'updatedDate'] }
+    const options = {
+      select: [
+        'id',
+        'cpf',
+        'firstName',
+        'lastName',
+        'email',
+        'createdDate',
+        'updatedDate',
+      ],
+    };
     return await this.usersRepository.save(user);
-
   }
 
   async delete(id: string) {
