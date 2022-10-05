6e7b7d13eeb271b12651bbc2ac0eb12207b582e4
arthurnassar <arthurnassar@gmail.com>
2022-10-05 23:11:24 +0100

    User sign in form layout and internal logic

--- front/new-website/src/pages/cadastro.vue
+++ front/new-website/src/pages/cadastro.vue
@@ -10,13 +10,20 @@ export default defineComponent({
   },
   data() {
     return {
-      userType: '' as string,
+      accountType: '' as string,
       firstName: '' as string,
       lastName: '' as string,
-      phone: '' as string,
+      phoneNumber: '' as string,
       email: '' as string,
       password: '' as string,
-      repeatPassword: '' as string
+      repeatPassword: '' as string,
+      cpf: '' as string,
+      sex: '' as string,
+      birthDate: '' as string,
+      state: '' as string,
+      city: '' as string,
+      emergencyContactName: '' as string,
+      emergencyContactPhoneNumber: '' as string
     }
   }
 })
@@ -42,22 +49,24 @@ export default defineComponent({
               <h1 class="text-gray-500 dark:text-gray-300">Escolha o tipo da conta</h1>
 
               <div class="mt-3 md:flex md:items-center md:-mx-2">
-                <input class="hidden" v-model="userType" type="radio" id="atleta" name="userType" value="atleta" />
-                <label for="atleta" :class="[ userType === 'atleta' ? 'text-white bg-blue-500' : 'text-blue-500' ]"
+                <input class="hidden" v-model="accountType" type="radio" id="atleta" name="accountType"
+                  value="atleta" />
+                <label for="atleta" :class="[ accountType === 'atleta' ? 'text-white bg-blue-500' : 'text-blue-500' ]"
                   class="flex justify-center w-full px-6 py-3 border border-blue-500 rounded-md md:w-auto md:mx-2 focus:outline-none">
-                  <UserCircleIcon class="h-6 w-6" :class="[ userType === 'atleta' ? 'text-white' : 'text-blue-500' ]" />
+                  <UserCircleIcon class="h-6 w-6"
+                    :class="[ accountType === 'atleta' ? 'text-white' : 'text-blue-500' ]" />
                   <span class="mx-2">
                     atleta
                   </span>
                 </label>
 
-                <input class="hidden" v-model="userType" type="radio" id="organizador" name="userType"
+                <input class="hidden" v-model="accountType" type="radio" id="organizador" name="accountType"
                   value="organizador" />
                 <label for="organizador"
-                  :class="[ userType === 'organizador' ? 'text-white bg-blue-500' : 'text-blue-500' ]"
+                  :class="[ accountType === 'organizador' ? 'text-white bg-blue-500' : 'text-blue-500' ]"
                   class="flex justify-center w-full px-6 py-3 mt-4 border border-blue-500 rounded-md md:mt-0 md:w-auto md:mx-2 focus:outline-none">
                   <BriefcaseIcon class="h-6 w-6"
-                    :class="[ userType === 'organizador' ? 'text-white' : 'text-blue-500' ]" />
+                    :class="[ accountType === 'organizador' ? 'text-white' : 'text-blue-500' ]" />
                   <span class="mx-2">
                     organizador
                   </span>
@@ -65,7 +74,7 @@ export default defineComponent({
               </div>
             </div>
 
-            <span class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+            <div class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
               <div>
                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nome</label>
                 <input autocomplete="given-name" v-model="firstName" type="text" placeholder="John"
@@ -80,7 +89,8 @@ export default defineComponent({
 
               <div>
                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Telefone</label>
-                <input autocomplete="tel" v-model="phone" type="text" placeholder="(XX) XXXXX-XXXX"
+                <input autocomplete="tel" v-model="phoneNumber" type="text" placeholder="(XX) XXXXX-XXXX"
+                  v-mask="'(00) 00000-0000'"
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
               </div>
 
@@ -103,13 +113,81 @@ export default defineComponent({
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
               </div>
 
-              <button
-                class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                <span>Cadastrar </span>
+              <div>
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">CPF</label>
+                <input v-model="cpf" type="text" placeholder="Enter your CPF" v-mask="'000.000.000-00'"
+                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+              </div>
 
-                <ChevronRightIcon class="h-6 w-6" />
-              </button>
-            </span>
+              <div class="md:flex flex-col md:items-start md:-mx-2">
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Sexo</label>
+                <div class="flex flex-row items-center">
+                  <input v-model="sex" type="radio" id="masculino" name="sex" value="masculino" />
+                  <label for="masculino">
+                    <span class="mx-2">
+                      Masculino
+                    </span>
+                  </label>
+                </div>
+
+                <div class="flex flex-row items-center">
+                  <input v-model="sex" type="radio" id="feminino" name="sex" value="feminino" />
+                  <label for="feminino">
+                    <span class="mx-2">
+                      Feminino
+                    </span>
+                  </label>
+                </div>
+              </div>
+
+              <div class="col-span-2">
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Data de nascimento</label>
+                <input v-model="birthDate" type="date" placeholder="Escolha uma data de nascimento"
+                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+              </div>
+
+
+
+              <div>
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Estado</label>
+                <select v-model="state"
+                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40">
+                  <option value="" default disabled>Escolha o estado</option>
+                </select>
+              </div>
+
+              <div>
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Cidade</label>
+                <select v-model="city"
+                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40">
+                  <option value="" selected disabled>Escolha a cidade</option>
+                </select>
+              </div>
+
+              <div>
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Contato de emergencia</label>
+                <input v-model="emergencyContactName" type="text" placeholder="Nome do contato de emergencia"
+                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+              </div>
+
+              <div>
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Telefone emergencia</label>
+                <input v-model="emergencyContactPhoneNumber" type="text" placeholder="(00) 00000-0000"
+                  v-mask="'(00) 00000-0000'"
+                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+              </div>
+
+            </div>
+
+
+
+            <button
+              class="flex items-center justify-between w-full px-6 py-3 mt-5 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+              <span>Cadastrar </span>
+
+              <ChevronRightIcon class="h-6 w-6" />
+            </button>
+
           </div>
         </div>
       </div>
